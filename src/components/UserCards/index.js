9b7dbb061c404750699a1330ccb7101a92ee63c4
Stephanie Fernandes <steph3001@gmail.com>
2020-02-08 17:21:02 -0300

    Add UserCards component

---
+++ src/components/UserCards/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import UserCard from '../UserCard/index'
+import './style.css';
+
+const UserCards = ({ filteredUsers }) => {
+  return (
+    <div className='user-list'>
+      { Object.keys(filteredUsers).map(user => (
+        <UserCard key={user} details={filteredUsers[user]} />
+      ))}
+    </div>
+  )
+}
+
+export default UserCards;
+
