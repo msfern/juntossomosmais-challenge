171163cf6b3c0321965ed610b2e0f9aa74a70b7b
Stephanie Fernandes <steph3001@gmail.com>
2020-02-08 17:21:42 -0300

    Add Pagination component

---
+++ src/components/Pagination/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css';
+
+const Pagination = () => {
+  return (
+    <nav className='pagination'>
+      Pagination
+    </nav>
+  )
+}
+
+export default Pagination
