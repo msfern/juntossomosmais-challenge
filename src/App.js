4a848f1d57dc376214f5fc085c9fd07c203edd9d
Stephanie Fernandes <steph3001@gmail.com>
2020-02-08 17:19:15 -0300

    Add components and states to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './components/Header/index';
+import FilterSideBar from './components/FilterSideBar/index';
+import UserCards from './components/UserCards/index';
+import Pagination from './components/Pagination/index';
+import Footer from './components/Footer/index';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
   return (
     <div className="App">
       <header className="App-header">
