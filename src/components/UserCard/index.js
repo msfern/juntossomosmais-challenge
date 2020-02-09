efa970796ede81999133748b1e65e7d6fa33edae
Stephanie Fernandes <steph3001@gmail.com>
2020-02-09 00:40:26 -0300

    Use Utils on UserCard

--- src/components/UserCard/index.js
+++ src/components/UserCard/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { formatString } from '../../utils/utils';
 import './style.css';
 
 export default function UserCard({ index, details }) {
-  function formatUserInfo(info) {
-    return info.split(' ').map(word => (
-      word.charAt(0).toUpperCase() + word.slice(1)
-    )).join(' ');
-  }
-
   return (
     <div className='user-card'>
       <div className='user-card__header'>
@@ -16,12 +11,12 @@ export default function UserCard({ index, details }) {
       </div>
       <div className='user-card__body'>
         <div className='middle'>
-          <p className='name-age'>{formatUserInfo(details.name.first)} {formatUserInfo(details.name.last)}, {details.dob.age}</p>
+          <p className='name-age'>{formatString(details.name.first)} {formatString(details.name.last)}, {details.dob.age}</p>
           <p className='email'>{details.email}</p>
         </div>
         <div className='bottom'>
-          <p className='location'>{formatUserInfo(details.location.city)}, {formatUserInfo(details.location.state)}</p>
-          <Link to={{pathname: `/users/${index}`, state: {details}}}>Ver mais</Link>
+          <p className='location'>{formatString(details.location.city)}, {formatString(details.location.state)}</p>
+          <Link className='go-to-details' to={{pathname: `/users/${index}`, state: {details}}}>Ver mais</Link>
         </div>
       </div>
     </div>
