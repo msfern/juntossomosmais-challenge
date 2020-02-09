a718aebd1d0af7a274735c78cf7098e1b6093294
Stephanie Fernandes <steph3001@gmail.com>
2020-02-09 00:46:23 -0300

    Add magnifying glass icon to SearchBar

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -5,6 +5,7 @@ const SearchBar = ({ updateSearch }) => {
   return (
     <div className='searchbar'>
       <input placeholder='Pesquisar usuário...' onChange={(e) => { updateSearch(e.target.value) }} />
+      <i className="fas fa-search"></i>
     </div>
   )
 }
