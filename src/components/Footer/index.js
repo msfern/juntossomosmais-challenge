5b4aafe3255437ec3b3c2451516031bb9450da43
Stephanie Fernandes <steph3001@gmail.com>
2020-02-09 22:48:01 -0300

    Update Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,12 @@ import './style.css';
 const Footer = () => {
   return (
     <footer className='footer'>
-      Footer
+      <img src='/logo.svg' alt='Logo' />
+      <p className='footer-text'>Acompanhe a gente nas redes sociais:</p>
+      <div className='social'>
+        <a className='social-item' href='https://www.facebook.com/OficialJuntosSomosMais/'><i className="fab fa-facebook-f"></i></a>
+        <a className='social-item' href='https://www.linkedin.com/company/juntos-somos-mais/'><i className="fab fa-linkedin-in"></i></a>
+      </div>
     </footer>
   )
 }
